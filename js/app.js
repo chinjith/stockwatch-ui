976c6a547ba89fe6307ac9c60421694c53dcfc6f
Chinjith Sasidharan <chinjith@gmail.com>
2021-07-31 02:33:37 +0530

    first code deposit to repo

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const container = document.querySelector(".container");
+
+const showCoffees = () => {
+
+};
+
+document.addEventListener("DOMContentLoaded", showCoffees);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
